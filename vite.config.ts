5897f86aa4f5045825ccf57930fec1b0aecb48e7
yasin7777-alt <yasinrakhimzhan@gmail.com>
2026-09-19 10:03:04 +0500

    Remove base URL from Vite configuration
    Removed the base URL configuration from vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig(async ({ mode }) => {
   }
 
   return { 
-    base: '/qazaq.tili/',
     plugins,
     envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
     define: processEnvDefines,
